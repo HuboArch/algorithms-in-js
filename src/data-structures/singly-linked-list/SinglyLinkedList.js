8eaa601a5e12a8bb0d674a7309abee850d06a5f3
agribeau <hkissbigapple@163.com>
2018-08-03 08:28:31 +0800

    feat(singly linked list): Add method for clearing list

--- src/data-structures/singly-linked-list/SinglyLinkedList.js
+++ src/data-structures/singly-linked-list/SinglyLinkedList.js
@@ -2,6 +2,22 @@ import Node from "SinglyLinkedListNode";
 
 export default class SinglyLinkedList {
     constructor() {
+        // 带有头结点的链表
         this.head = new Node();
+    }
+
+    /**
+     * 清空链表
+     */
+    clearList() {
+        let pNode   = this.head.next,
+            tmpCell = null;
+
+        while (pNode !== null) {
+            tmpCell = pNode.next;
+            pNode   = null;
+            pNode   = tmpCell;
+        }
+        this.head.next = null;
     }
 }
