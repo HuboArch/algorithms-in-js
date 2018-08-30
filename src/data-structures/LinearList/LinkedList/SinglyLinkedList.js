ef80a813b95610c6ce381837339d4b928d3a152a
agribeau <hkissbigapple@163.com>
2018-08-30 13:05:50 +0800

    feat(singly linked list): Add methods for inserting an element into the list

--- src/data-structures/LinearList/LinkedList/SinglyLinkedList.js
+++ src/data-structures/LinearList/LinkedList/SinglyLinkedList.js
@@ -7,7 +7,6 @@ export default class SinglyLinkedList {
      * @param [comparatorFunc] {Function}
      */
     constructor(comparatorFunc) {
-        /** @var SinglyLinkedListNode */
         this.dummyHead = new Node();
         this.size = 0;
 
@@ -31,4 +30,49 @@ export default class SinglyLinkedList {
     isEmpty() {
         return this.size === 0;
     }
+
+    /**
+     * 在指定的索引位置添加元素
+     *
+     * @param idx {number}
+     * @param elem {*}
+     */
+    add(idx, elem) {
+        if (idx < 0 || idx > this.size) {
+            throw new Error("Add failed. Illegal argument idx.");
+        }
+
+        prev = this.dummyHead;
+        for (let i = 0; i < idx; i++) {
+            prev = prev.next;
+        }
+
+        let tmpCell = prev.next;
+        prev.next = new Node(elem, tmpCell);
+        if (tmpCell !== null) {
+            tmpCell.next = null; // prevent loitering object
+        }
+
+        this.size++;
+    }
+
+    /**
+     * 在链表首部添加元素
+     *
+     * @param elem {*}
+     */
+    addFirst(elem) {
+        this.add(0, elem);
+    }
+
+    /**
+     * 在链表尾部添加元素
+     *
+     * @param elem {*}
+     */
+    addLast(elem) {
+        this.add(this.size, elem);
+    }
+
+
 }
