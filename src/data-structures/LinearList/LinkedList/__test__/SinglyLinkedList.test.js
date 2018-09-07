a03b31ebcb1dd0fc259d1562bffb384a03696a8d
agribeau <hkissbigapple@163.com>
2018-09-07 23:59:50 +0800

    test(singly linked list): Update SinglyLinkedList addLast test

--- src/data-structures/LinearList/LinkedList/__test__/SinglyLinkedList.test.js
+++ src/data-structures/LinearList/LinkedList/__test__/SinglyLinkedList.test.js
@@ -8,4 +8,14 @@ describe('SinglyLinkedList', () => {
         expect(linkedList.toString()).toBe('');
     });
 
+    it('should add an node at the rear of list', () => {
+        const linkedList = new LinkedList();
+
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
+
+        expect(linkedList.toString()).toBe('1,2,3');
+    });
+
 });
